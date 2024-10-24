f3e41c3336e0315f1b943ffd609fc21d7527fa69
rmcrossley <becky.crossley@dhsc.gov.uk>
2024-10-24 10:19:55 +0100

    Updated to work as in HSE2019 version

--- R/run_analysis.R
+++ R/run_analysis.R
@@ -1,12 +1,10 @@
 
 # Load required packages --------------------------------------------------
 library(tidyverse)
-
+library(vcd)
 
 # Source other scripts ----------------------------------------------------
-download <- new.env(); source("./R/download.R", local = download)
-process <- new.env(); source("./R/process.R", local = process)
-visualise <- new.env(); source("./R/visualise.R", local = visualise)
+upload <- new.env(); source("./R/upload.R", local = upload)
 
 
 # Main function to run analysis -------------------------------------------
@@ -19,99 +17,94 @@ visualise <- new.env(); source("./R/visualise.R", local = visualise)
 #' @export
 #'
 run_analysis <- function() {
-  # log action
+  print("Found the run_analysis function!")
+  #log action
   logger$info("Running analysis...")
 
-  # read in configuration
-  config_path <- file.path("input", "config.yml")
-  config <- yaml::read_yaml(config_path)
+  # Load in data
+  df <- upload$hse_2019_in_lab
 
-  # get file date and time stamp
-  config$date_stamp <- format(Sys.time(), "%Y%m%d-%H%M")
+  # Loop through factors for plots --------------------------------------------------------------------------------------
+  # BMI first
+  # List of factors to iterate through
+  factors <- c("ag16g10", "Sex", "ThCoAny", "origin2",
+               "LifeSatG", "wemwbs", "IllAff7", "ILL12m", "MENHTAKg2",
+               "AntiDepTakg2", "SCOFF2", "qimd19", "AntiDepM2", "topqual3", "RELIGSC", "HHINC3", "eqv5", "totalwug_19")
 
-  # download summary file
-  attendance_data <-
-    download$download_nhsd_data(
-      config$source_url,
-      config$summary$regex,
-      file.path(
-        "input",
-        sprintf("%s_%s", config$date_stamp, config$summary$filename)
-      )
-    ) %>%
-    process$load_attendance_data()
+  # Initialize a data frame to store results
+  results <- data.frame(Factor = character(), Chi_Square_p_value = numeric(),
+                        Cramers_V = numeric(), stringsAsFactors = FALSE)
 
-  visualise$save_plot(
-    visualise$plot_attendance_proportions(attendance_data),
-    file.path(
-      "output",
-      sprintf("%s_%s", config$date_stamp, "attendance_plot.svg")
-    )
-  )
+  # Loop through each factor and perform the analysis
+  for (factor in factors) {
+    print(paste("Analysing", factor, "vs BMI"))
 
-  # download summary file
-  nims_data <-
-    download$download_nhsd_data(
-      config$source_url,
-      config$nims$regex,
-      file.path(
-        "input",
-        sprintf("%s_%s", config$date_stamp, config$nims$filename)
-      )
-    ) %>%
-    process$load_nims_data()
+    # Clean out NAs for both Sex and the current factor
+    df_clean <- df[complete.cases(df$BMIvg5, df[[factor]]), ]
 
-  # pick region of interest and only select full years
-  nims_monthly_eng <-
-    process$get_monthly_totals(
-      nims_data %>% filter(area_name == "England")
-    ) %>%
-    group_by(year) %>%
-    filter(n() == 12) %>%
-    ungroup()
+    # Create a contingency table
+    tbl <- table(df_clean$BMIvg5, df_clean[[factor]])
 
-  visualise$save_plot(
-    visualise$plot_nims_monthly(nims_monthly_eng),
-    file.path(
-      "output",
-      sprintf("%s_%s", config$date_stamp, "nims_monthly_eng.svg")
-    )
-  )
+    # Perform chi-square test
+    chi_square_result <- chisq.test(tbl)
 
-  # NIMS data uses old ICB codes so need to update to new 2023
-  # codes
-  nims_icb_data <- nims_data %>%
-    filter(
-      area_type == "ICB",
-      date == as.Date("2023-01-01")
-    ) %>%
-    mutate(
-      ons_code = case_when(
-        ons_code == "E54000053" ~ "E54000064",
-        ons_code == "E54000052" ~ "E54000063",
-        TRUE ~ ons_code
-      )
-    ) %>%
-    select(ons_code, total)
+    # Calculate Cramér's V
+    cramers_v <- assocstats(tbl)$cramer
 
+    # Save the results
+    results <- rbind(results, data.frame(Factor = factor,
+                                         Chi_Square_p_value = chi_square_result$p.value,
+                                         Cramers_V = cramers_v))
 
-  visualise$save_plot(
-    visualise$plot_nims_icb_map(nims_icb_data),
-    file.path(
-      "output",
-      sprintf("%s_%s", config$date_stamp, "nims_icb_map_20230101.svg")
-    )
-  )
+    # Visualize with a mosaic plot
+    mosaicplot(tbl, main = paste("Mosaic Plot of", factor, "and BMI"),
+               ylab = factor, xlab = "BMI", color = TRUE)
+  }
 
-  # copy config to output
-  fs::file_copy(
-    file.path("input", "config.yml"),
-    file.path(
-      "output",
-      sprintf("%s_config.yml", config$date_stamp)
-    ),
-    overwrite = TRUE
-  )
+  # Print or save the results
+  print(results)
 
-  logger$info("Finished")
+  # Optionally, save results to a CSV or document
+  write.csv(results, "BMI_chi_square_results.csv", row.names = FALSE)
+
+  # BMI first
+  # List of factors to iterate through
+  factors <- c("ag16g10", "Sex", "qimd19", "origin2", "topqual3", "RELIGSC", "HHINC3", "eqv5", "totalwug_19")
+
+  # Initialize a data frame to store results
+  results <- data.frame(Factor = character(), Chi_Square_p_value = numeric(),
+                        Cramers_V = numeric(), stringsAsFactors = FALSE)
+
+  # Loop through each factor and perform the analysis
+  for (factor in factors) {
+    print(paste("Analysing", factor, "vs Life Satisfaction"))
+
+    # Clean out NAs for both Sex and the current factor
+    df_clean <- df[complete.cases(df[[factor]], df$LifeSatG), ]
+
+    # Create a contingency table
+    tbl <- table(df_clean[[factor]], df_clean$LifeSatG)
+
+    # Perform chi-square test
+    chi_square_result <- chisq.test(tbl)
+
+    # Calculate Cramér's V
+    cramers_v <- assocstats(tbl)$cramer
+
+    # Save the results
+    results <- rbind(results, data.frame(Factor = factor,
+                                         Chi_Square_p_value = chi_square_result$p.value,
+                                         Cramers_V = cramers_v))
+
+    # Visualize with a mosaic plot
+    mosaicplot(tbl, main = paste("Mosaic Plot of", factor, "and Life Satisfaction"),
+               xlab = factor, ylab = "Life Satisfaction", color = TRUE)
+  }
+
+  # Print or save the results
+  print(results)
+
+  # Optionally, save results to a CSV or document
+  write.csv(results, "LifeSatG_chi_square_results.csv", row.names = FALSE)
+
 }
---
+++ R/upload.R
@@ -0,0 +1,53 @@
+
+# Load required packages --------------------------------------------------
+library(tidyverse)
+library(haven)
+
+hse_2019_in <- read_sav("C:/Users/Becky.Crossley/Department of Health and Social Care/GOV-Healthy Weight, Food & Nutrition Analysis - Documents/Data/HSfE/Raw/2019/UKDA-8860-spss/spss/spss25/hse_2019_eul_20211006.sav")
+
+# Get the column names
+#column_names <- colnames(hse_2019_in)
+
+# Save the column names to a text file
+#write(column_names, file = "column_names_hse_2019.txt")
+
+# print(head(hse_2019_in))
+
+# Reduce to data we want --------------------------------------------
+# Select the specific columns
+hse_2019_in_reduced <- hse_2019_in %>%
+  select(Sex, ag16g10, HSEYR, BMIvg5, ThCoAny, SCSatis, origin2, LifeSatG, wemwbs, IllAff7, ILL12m, MENHTAKg2, AntiDepTakg2, SCOFF2, qimd19, AntiDepM2, topqual3, RELIGSC, HHINC3, eqv5, totalwug_19)
+
+#print(head(hse_2019_in_reduced))
+#print(summary(hse_2019_in_reduced))
+
+hse_2019_in_lab <- hse_2019_in_reduced
+
+# Clean data to label correctly
+# Modify factor levels for BMI
+hse_2019_in_lab$BMIvg5 <- factor(hse_2019_in_lab$BMIvg5, levels = c("1", "2", "3", "4", "5"),
+                                 labels = c("Underweight", "Normal", "Overweight", "Obese", "Morbidly Obese"))
+# Modify factor levels for Sex
+hse_2019_in_lab$Sex <- factor(hse_2019_in_lab$Sex, levels = c("1", "2"), labels = c("Female", "Male"))
+# Modify factor levels for Age groups
+hse_2019_in_lab$ag16g10 <- factor(hse_2019_in_lab$ag16g10, levels = c("1", "2", "3", "4", "5", "6", "7"), labels = c("16-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75+"))
+# Modify factor levels for Therapy
+hse_2019_in_lab$ThCoAny <- factor(hse_2019_in_lab$ThCoAny, levels = c("0", "1"), labels = c("Not mentioned", "Mentioned"))
+# Modify factor levels for life satisfaction
+hse_2019_in_lab$LifeSatG <- factor(hse_2019_in_lab$LifeSatG, levels = c("1", "2", "3", "4"), labels = c("Low (0-4)", "Medium (5-6)", "High (7-8)", "Very high (9-10)"))
+# Modify factor levels for whether mental health drug was taken in last week
+hse_2019_in_lab$MENHTAKg2 <- factor(hse_2019_in_lab$MENHTAKg2, levels = c("0", "1"), labels = c("0", "1+"))
+# Modify factor levels for whether prescribed drug for depression was taken in last week
+hse_2019_in_lab$AntiDepTakg2 <- factor(hse_2019_in_lab$AntiDepTakg2, levels = c("0", "1"), labels = c("0", "1+"))
+# Modify factor levels for whether prescribed drug for depression
+hse_2019_in_lab$AntiDepM2 <- factor(hse_2019_in_lab$AntiDepM2, levels = c("0", "1"), labels = c("Not taking", "Taking"))
+# Modify factor levels for ED screener, SCOFF
+hse_2019_in_lab$SCOFF2 <- factor(hse_2019_in_lab$SCOFF2, levels = c("1", "2"), labels = c("Score 0-1", "Score 2+"))
+# Modify factor levels for depravity
+hse_2019_in_lab$qimd19 <- factor(hse_2019_in_lab$qimd19, levels = c("1", "5"), labels = c("Least Deprived", "Most Deprived"))
+# Modify factor levels for highest level of qualification
+hse_2019_in_lab$topqual3 <- factor(hse_2019_in_lab$topqual3, levels = c("1", "2", "3", "4", "5", "6", "7"), labels = c("NVQ4/NVQ5/Degree or equiv", "Higher ed below degree", "NVQ3/GCE A Level equiv", "NVQ2/GCE O Level equiv", "NVQ1/CSE other grade equiv", "Foreign/other", "No qualification"))
+# Modify factor levels for quintiles for total household income
+hse_2019_in_lab$eqv5 <- factor(hse_2019_in_lab$eqv5, levels = c("1", "2", "3", "4", "5"), labels = c( "Lowest Quintile (<=£14,956)", "Second lowest Quintile (>£14,956 <= £23,443)", "Middle Quintile (>£23,443 <=£35,540)", "Second highest Quintile (>£35,540 <=£56,000)", "Highest Quintile (>£56,000)"))
+# Modify factor levels for alcohol units per week
+hse_2019_in_lab$totalwug_19 <- factor(hse_2019_in_lab$totalwug_19, levels = c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"), labels = c("None drinker/not in last 12 months", "Non-zero, but under 1", "1-7", "Over 7-10", "Over 10-14", "Over 14-21", "Over 21-28", "Over 28-35", "Over 35-50", "Over 50"))
